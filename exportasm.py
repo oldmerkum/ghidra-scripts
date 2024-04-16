b738ba550562d28197ebb7223f9d5bb10a272308
Antonio Marcum <39196945+oldmerkum@users.noreply.github.com>
2024-04-16 16:36:54 -0400

    Update exportasm.py

--- exportasm.py
+++ exportasm.py
@@ -1,10 +1,11 @@
 #Export assembly instructions for functions of a binary
 #@author oldmerkum
 #@category assembly
-#@keybinding 
+#@keybinding
 #@menupath Tools.Misc.Export asm functions
-#@toolbar 
+#@toolbar
 
+from ghidra.program.model.block import BasicBlockModel
 import time
 
 asm_filename = askFile("asm filename", "filename")
@@ -16,6 +17,8 @@ with open(asm_filename.getPath(), "w") as asm_outfile:
   fxn_manager = currentProgram.getFunctionManager()
   code_manager = currentProgram.getCodeManager()
 
+  code_block_model = BasicBlockModel(currentProgram)
+
   monitor.initialize(fxn_manager.getFunctionCount())
 
   current_count = 1
@@ -24,8 +27,11 @@ with open(asm_filename.getPath(), "w") as asm_outfile:
     monitor.setMessage("Working on function " + str(current_count) + " " + fxn.getName())
 
     asm_outfile.write(';' + fxn.getName() + "\n")
-    for instr in code_manager.getInstructions(fxn.getBody(), True):
-      asm_outfile.write(instr.toString() + "\n")
+
+    for block in code_block_model.getCodeBlocksContaining(fxn.getBody(), monitor):
+      asm_outfile.write('::block\n')
+      for instr in code_manager.getInstructions(block, True):
+        asm_outfile.write(instr.toString() + "\n")
 
     monitor.checkCanceled()
     monitor.incrementProgress(1)
